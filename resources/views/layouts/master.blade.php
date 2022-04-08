515c9daa5a559c910108f35a4a5200c0562f0ff8
abuzar-gifari <shaikh35-3024@diu.edu.bd>
2022-04-08 10:59:00 +0600

    template mastering done

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,27 @@
+@includeIf('layouts.partials._head')
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+    @include('layouts.partials._navbar')
+
+  <!-- Main Sidebar Container -->
+    @include('layouts.partials._sidebar')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+
+
+  </div>
+  <!-- /.content-wrapper -->
+
+
+  <!-- Main Footer -->
+@include('layouts.partials._footer')
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+@include('layouts.partials._script')
+</body>
+</html>
